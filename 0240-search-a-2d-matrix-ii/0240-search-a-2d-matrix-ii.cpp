d12fa491984529f2db045b01a99be47888e5c19b
Bhuvesh-Garg <bhuveshgarg990@gmail.com>
2026-07-08 12:41:06 +0530

    Time: 59 ms (30.44%), Space: 18.7 MB (67.92%) - LeetHub

--- 0240-search-a-2d-matrix-ii/0240-search-a-2d-matrix-ii.cpp
+++ 0240-search-a-2d-matrix-ii/0240-search-a-2d-matrix-ii.cpp
@@ -5,19 +5,10 @@ public:
         int m=mat[0].size();
         int row= 0,col=m-1;
         while(row<n&&col>=0){
-        
-            if(mat[row][col]==target){
-                return true;
+            if(mat[row][col]==target) return true;
+            else if(mat[row][col]<target)   row++;
+            else col--;
             }
-            else if(mat[row][col]<target){
-                row++;;
-            }
-            else{
-                col--;
-            
-            }
-       
-        }
-     return false;
+        return false;
     }
 };
