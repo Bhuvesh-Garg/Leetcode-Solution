9f85f9f256e79402a6c2b417ec8b28217486c58f
Bhuvesh-Garg <bhuveshgarg990@gmail.com>
2026-07-11 14:58:31 +0530

    Time: 14 ms (99.78%), Space: 9.3 MB (85.39%) - LeetHub

--- 1781-sum-of-beauty-of-all-substrings/1781-sum-of-beauty-of-all-substrings.cpp
+++ 1781-sum-of-beauty-of-all-substrings/1781-sum-of-beauty-of-all-substrings.cpp
@@ -7,16 +7,29 @@ public:
         
         for (int i = 0; i < n; i++) {
             int freq[26] = {0};
+            int countOfFreq[501] = {0};
+            
+            int maxFreq = 0;
+            int minFreq = 0;
             
             for (int j = i; j < n; j++) {
-                freq[s[j] - 'a']++;
-                int maxFreq = 0;
-                int minFreq = n;
-                for (int k = 0; k < 26; k++) {
-                    if (freq[k] > 0) {
-                        maxFreq = max(maxFreq, freq[k]);
-                        minFreq = min(minFreq, freq[k]);
-                    }
+                int idx = s[j] - 'a';
+                int oldFreq = freq[idx];
+                int newFreq = oldFreq + 1;
+                
+                freq[idx] = newFreq;
+                
+                if (oldFreq > 0) {
+                    countOfFreq[oldFreq]--;
+                }
+                countOfFreq[newFreq]++;
+                
+                maxFreq = max(maxFreq, newFreq);
+                
+                if (oldFreq == minFreq && countOfFreq[oldFreq] == 0) {
+                    minFreq = newFreq;
+                } else if (newFreq < minFreq || minFreq == 0) {
+                    minFreq = newFreq;
                 }
                 
                 totalBeauty += (maxFreq - minFreq);
